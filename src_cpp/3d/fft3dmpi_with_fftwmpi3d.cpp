5acbe5fb54bf015310ab1a51d83d07b3f8c1af86
Cyrille Bonamy <cyrille.bonamy@legi.cnrs.fr>
2018-02-08 10:50:36 +0100

    update are_parameters_bad for fftwmpi3d class

--- src_cpp/3d/fft3dmpi_with_fftwmpi3d.cpp
+++ src_cpp/3d/fft3dmpi_with_fftwmpi3d.cpp
@@ -325,10 +325,10 @@ void FFT3DMPIWithFFTWMPI3D::get_seq_indices_first_K(int *i0, int *i1)
 
 bool FFT3DMPIWithFFTWMPI3D::are_parameters_bad()
 {
-  if ((N0*N1)/nb_proc == 0)
+  if (N0/nb_proc == 0)
     {
       if (rank == 0)
-        cout << "bad parameters N0 or N1 or N2" <<endl;
+        cout << "bad parameters N0 : nb_proc>N0 (not supported by fftwmpi)" <<endl;
         return 1;
     }
   return 0;
