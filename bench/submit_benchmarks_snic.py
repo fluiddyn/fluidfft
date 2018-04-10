a340331361df000fcc4e4c0c08802d315ca643a1
Ashwin Vishnu <avmo@kth.se>
2018-04-10 17:55:21 +0200

    Use constrained Beskow36 cluster, more compatible no. of nodes with bench scripts

--- bench/submit_benchmarks_snic.py
+++ bench/submit_benchmarks_snic.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import os
 import numpy as np
-from fluiddyn.clusters.snic import ClusterSNIC as Cluster
+# from fluiddyn.clusters.snic import ClusterSNIC as Cluster
+from fluiddyn.clusters.snic import Beskow36 as Cluster
 
 
 # Parameters
 # ----------
-## n0 = 2 ** 10; 'Triolith / Beskow'
-n = (384, 1152, 1152); nb_cores = [2, 4, 8, 16, 32]; nodes = [1, 2, 4, 8, 16, 32, 64, 128]
+## 'Triolith / Beskow'
+# n = (384, 1152, 1152); nb_cores = [2, 4, 8, 16, 32]; nodes = [1, 2, 4, 6, 8, 9, 12, 16, 18, 32, 36]
+n = (1152, 1152, 1152); nb_cores = [2, 4, 8, 16, 32]; nodes = [1, 2, 4, 6, 8, 9, 12, 16, 18, 32, 36, 64]  # , 128, 256, 384, 512]
 
-## n0 = 2**6 * 3**2 * 7; 'Kebnekaise'
+## 'Kebnekaise'
 # n = (1008,); nb_cores = [2, 4, 8, 12, 16, 21, 24, 28]; nodes = [2, 3, 4, 6]
 
 
@@ -19,10 +21,12 @@ def shape(join_with=' '):
 
 
 # argv = dict(dim='2d', nh=f'{shape()} -d 2', time='00:04:00')  # 2D benchmarks
-argv = dict(dim='3d', nh=f'{shape()} -d 3', time='00:20:00')  # 3D benchmarks
+argv = dict(dim='3d', nh=f'{shape()} -d 3', time='01:00:00')  # 3D benchmarks
 # mode = 'intra'
 mode = 'inter'
 # mode = 'inter-intra'
+dry_run = False
+# dry_run = True
 
 
 def init_cluster():
@@ -33,10 +37,12 @@ def init_cluster():
         f"./../../fluidfft-bench-results/"
         f"{cluster.name_cluster}_{shape('x')}")
 
-    cluster.max_walltime = '00:20:01'
+    cluster.max_walltime = argv['time']
     if cluster.name_cluster == 'beskow':
         cluster.default_project = '2017-12-20'
-        interactive=False
+        interactive = True
+        cluster.nb_cores_per_node = 32  # TODO: Change this if other shapes are used
+        cluster.cmd_run_interactive = f'aprun -N {cluster.nb_cores_per_node}'
         cluster.commands_unsetting_env.insert(
             0, f'aprun -n 1 fluidinfo -o {output_dir}')
     else:
@@ -57,14 +63,18 @@ def submit(cluster, interactive, nb_nodes, nb_cores_per_node=None):
         nb_cores_per_node = cluster.nb_cores_per_node
 
     nb_mpi = nb_cores_per_node * nb_nodes
-    cluster.submit_command(
-        f"fluidfft-bench {argv['nh']} -o {output_dir} -n 20",
-        name_run=f"fft{argv['dim']}_{nb_mpi}",
-        nb_nodes=nb_nodes,
-        nb_cores_per_node=nb_cores_per_node,
-        walltime=argv['time'],
-        nb_mpi_processes=nb_mpi, omp_num_threads=1,
-        ask=False, bash=False, interactive=interactive)
+    cmd = f"fluidfft-bench {argv['nh']} -o {output_dir} -n 20"
+    if dry_run:
+        print(f'np={nb_mpi}', cmd)
+    else:
+        cluster.submit_command(
+            cmd,
+            name_run=f"fft{argv['dim']}_{nb_mpi}",
+            nb_nodes=nb_nodes,
+            nb_cores_per_node=nb_cores_per_node,
+            walltime=argv['time'],
+            nb_mpi_processes=nb_mpi, omp_num_threads=1,
+            ask=False, bash=False, interactive=interactive)
 
 
 cluster, interactive = init_cluster()
