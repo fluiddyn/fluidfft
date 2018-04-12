5a9dae764ac2aeee3b67310bfbd91b16ff5ccf67
Ashwin Vishnu <avmo@kth.se>
2018-04-12 12:11:05 +0200

    Benchmark Beskow32 cluster

--- bench/submit_benchmarks_snic.py
+++ bench/submit_benchmarks_snic.py
@@ -2,14 +2,14 @@
 import os
 import numpy as np
 # from fluiddyn.clusters.snic import ClusterSNIC as Cluster
-from fluiddyn.clusters.snic import Beskow36 as Cluster
+from fluiddyn.clusters.snic import Beskow32 as Cluster
 
 
 # Parameters
 # ----------
 ## 'Triolith / Beskow'
-n = (384, 1152, 1152); nb_cores = [2, 4, 8, 16, 24]; nodes = [1, 2, 4, 16, 20, 40, 50]  # Similar to Occigen
-# n = (384, 1152, 1152); nb_cores = [2, 4, 8, 16, 32];  nodes = [1, 2, 4, 6, 8, 9, 12, 16, 18, 32, 36, 64]
+# n = (384, 1152, 1152); nb_cores = [2, 4, 8, 16, 24]; nodes = [1, 2, 4, 16, 20, 40, 50]  # Similar to Occigen
+n = (384, 1152, 1152); nb_cores = [2, 4, 8, 16, 32];  nodes = [1, 2, 4, 6, 8, 9, 12, 16, 18, 32, 36]
 # n = (1152, 1152, 1152); nb_cores = [2, 4, 8, 16, 32]; nodes = [1, 2, 4, 6, 8, 9, 12, 16, 18, 32, 36, 64]  # , 128, 256, 384, 512]
 
 ## 'Kebnekaise'
@@ -25,8 +25,8 @@ def shape(join_with=' '):
 argv = dict(dim='3d', nh=f'{shape()} -d 3', time='00:20:00')  # 3D benchmarks small
 # argv = dict(dim='3d', nh=f'{shape()} -d 3', time='01:00:00')  # 3D benchmarks large
 # mode = 'intra'
-mode = 'inter'
-# mode = 'inter-intra'
+# mode = 'inter'
+mode = 'inter-intra'
 dry_run = False
 # dry_run = True
 
@@ -37,13 +37,13 @@ def init_cluster():
     cluster = Cluster()
     output_dir = os.path.abspath(
         f"./../../fluidfft-bench-results/"
-        f"{cluster.name_cluster}24_{shape('x')}")
+        f"{cluster.name_cluster}32_{shape('x')}")
 
     cluster.max_walltime = argv['time']
     if cluster.name_cluster == 'beskow':
         cluster.default_project = '2017-12-20'
         interactive = True
-        cluster.nb_cores_per_node = 24  # TODO: Change this if other shapes are used
+        cluster.nb_cores_per_node = 32  # TODO: Change this if other shapes are used
         cluster.cmd_run_interactive = f'aprun -N {cluster.nb_cores_per_node}'
         cluster.commands_unsetting_env.insert(
             0, f'aprun -n 1 fluidinfo -o {output_dir}')
