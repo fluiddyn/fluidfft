c604b9ad0e8f2b3802d008a668a32274b140137e
paugier <pierre.augier@univ-grenoble-alpes.fr>
2020-06-23 15:49:05 +0200

    Update bench projperpk3d_inplace

--- bench/projperpk3d_inplace/bench.py
+++ bench/projperpk3d_inplace/bench.py
@@ -1,28 +1,36 @@
-
 import numpy as np
 
-# from proj_native import proj as proj_native
-# from proj_default import proj as proj_default
-# try:
-#     from proj_omp import proj as proj_omp
-# except ImportError:
-#     pass
-
-from proj_native import proj_loop as proj_native
-from proj_default import proj_loop as proj_default
 try:
-    from proj_omp import proj_loop as proj_omp
+    from proj_native import proj as proj_native
+    from proj_native import proj_loop as proj_loop_native
+except ImportError:
+    pass
+
+try:
+    from proj_default import proj as proj_default
+    from proj_default import proj_loop as proj_loop_default
+except ImportError:
+    pass
+
+try:
+    from proj_omp import proj as proj_omp
+    from proj_omp import proj_loop as proj_loop_omp
 except ImportError:
     pass
 
 
 from proj import proj as proj_py
 
-from fluidfft.fft3d.util_pythran import project_perpk3d as proj_fft
+# from fluidfft.fft3d.util_pythran import project_perpk3d as proj_fft
+# from fluidfft.fft3d.operators import OperatorsPseudoSpectral3D
 
 n0 = n1 = n2 = 128
+# lx = 2*np.pi
 
-shape = (n0, n1, n2//2+1)
+# oper = OperatorsPseudoSpectral3D(n2, n1, n0, lx, lx, lx)
+# proj_fft =
+
+shape = (n0, n1, n2 // 2 + 1)
 
 c0 = 1.3j + np.ones(shape, dtype=np.complex128)
 c1 = 2.3j + np.ones(shape, dtype=np.complex128)
