6283864a2031132608e50003f4d9c607ad002d34
ogiorgis <olivier.giorgis@logilab.fr>
2024-01-30 14:46:09 +0100

    ci: run black on plugins

--- plugins/fluidfft-build-deps/fluidfft_build_deps/src_cpp/2d/run_benchs.py
+++ plugins/fluidfft-build-deps/fluidfft_build_deps/src_cpp/2d/run_benchs.py
@@ -1,11 +1,10 @@
-
 import os
 
 import subprocess32 as subprocess
 
 import numpy as np
 
-output_dir = 'Results_bench'
+output_dir = "Results_bench"
 
 
 def call_bash(commands):
@@ -25,22 +24,22 @@ classes = []
 
 for i, nb_proc in enumerate(nb_procs_all):
 
-    command = './test_bench.out --N0={} --N1={}'.format(N0, N1)
+    command = "./test_bench.out --N0={} --N1={}".format(N0, N1)
 
     if nb_proc != 1:
-        command = 'mpirun -np {} '.format(nb_proc) + command
+        command = "mpirun -np {} ".format(nb_proc) + command
 
     txt = call_bash(command)
 
-    blocks = txt.split('--------')
+    blocks = txt.split("--------")
     # print('\n'.join(lines))
 
     for block in blocks:
-        if 'Initialization' in block:
+        if "Initialization" in block:
             lines = block.splitlines()
             for line in lines:
-                if line.startswith('Initialization ('):
-                    cls = line.split('(')[1].split(')')[0]
+                if line.startswith("Initialization ("):
+                    cls = line.split("(")[1].split(")")[0]
                     if cls not in classes:
                         classes.append(cls)
                         nb_procs[cls] = []
@@ -48,16 +47,20 @@ for i, nb_proc in enumerate(nb_procs_all):
                         times_ifft[cls] = []
 
             for line in lines:
-                if line.startswith('nb_proc:'):
+                if line.startswith("nb_proc:"):
                     nb_procs[cls].append(int(line.split()[1]))
-                if line.startswith('time fft'):
+                if line.startswith("time fft"):
                     times_fft[cls].append(float(line.split()[3]))
-                if line.startswith('time ifft'):
+                if line.startswith("time ifft"):
                     times_ifft[cls].append(float(line.split()[3]))
 
-            print('class ' + cls +
-                  ': times fft and ifft: {:5.3f} ; {:5.3f}'.format(
-                      times_fft[cls][-1], times_ifft[cls][-1]))
+            print(
+                "class "
+                + cls
+                + ": times fft and ifft: {:5.3f} ; {:5.3f}".format(
+                    times_fft[cls][-1], times_ifft[cls][-1]
+                )
+            )
 
 src = """
 from numpy import array
@@ -69,12 +72,22 @@ times_ifft = {}
 
 for cls in classes:
     src += (
-        "\nnb_procs['" + cls + "'] = " + repr(np.array(nb_procs[cls])) +
-        "\ntimes_fft['" + cls + "'] = " + repr(np.array(times_fft[cls])) +
-        "\ntimes_ifft['" + cls + "'] = " + repr(np.array(times_ifft[cls])))
+        "\nnb_procs['"
+        + cls
+        + "'] = "
+        + repr(np.array(nb_procs[cls]))
+        + "\ntimes_fft['"
+        + cls
+        + "'] = "
+        + repr(np.array(times_fft[cls]))
+        + "\ntimes_ifft['"
+        + cls
+        + "'] = "
+        + repr(np.array(times_ifft[cls]))
+    )
 
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
-with open(output_dir + '/bench.py', 'w') as f:
-    f.write(src + '\n')
+with open(output_dir + "/bench.py", "w") as f:
+    f.write(src + "\n")
