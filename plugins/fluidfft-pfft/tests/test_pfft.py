34bc5d8c512ec8e1e1d19295dbf1aacc785b317a
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-02-08 15:41:32 +0100

    Skip pfft tests if the class can not imported

--- plugins/fluidfft-pfft/tests/test_pfft.py
+++ plugins/fluidfft-pfft/tests/test_pfft.py
@@ -7,4 +7,5 @@ class Tests(TestCase):
     pass
 
 
-complete_test_class_3d("fft3d.mpi_with_pfft", Tests)
+# Importing pfft can fail because of CPU incompatibility
+complete_test_class_3d("fft3d.mpi_with_pfft", Tests, skip_if_import_error=False)
