50df6b93286648a9058e32081d3427273bd42f78
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-08-22 14:22:47 +0200

    FLUIDFFT_DISABLE_IMPORT_CHECK

--- src/fluidfft/__init__.py
+++ src/fluidfft/__init__.py
@@ -177,6 +177,9 @@ def _check_failure(method):
     if not any(method.endswith(postfix) for postfix in ("pfft", "p3dfft")):
         return False
 
+    if os.environ.get("FLUIDFFT_DISABLE_IMPORT_CHECK"):
+        return False
+
     # for few methods, try before real import because importing can lead to
     # a fatal error (Illegal instruction)
     if mpi.rank == 0:
